eb07b396515521495e69b052024e163e08932488
Peter Yoon <peyo.hd@gmail.com>
2021-03-29 14:59:01 +0900

    v3d_bufmgr : print log in v3d_bo_map_unsynchronized()

--- src/gallium/drivers/v3d/v3d_bufmgr.c
+++ src/gallium/drivers/v3d/v3d_bufmgr.c
@@ -525,14 +525,14 @@ v3d_bo_map_unsynchronized(struct v3d_bo *bo)
         ret = v3d_ioctl(bo->screen->fd, DRM_IOCTL_V3D_MMAP_BO, &map);
         offset = map.offset;
         if (ret != 0) {
-                fprintf(stderr, "map ioctl failure\n");
+                ALOGE("v3d_bo_map_unsynchronized() map ioctl failure");
                 abort();
         }
 
         bo->map = mmap(NULL, bo->size, PROT_READ | PROT_WRITE, MAP_SHARED,
                        bo->screen->fd, offset);
         if (bo->map == MAP_FAILED) {
-                fprintf(stderr, "mmap of bo %d (offset 0x%016llx, size %d) failed\n",
+                ALOGE("v3d_bo_map_unsynchronized() mmap of bo %d (offset 0x%016llx, size %d) failed",
                         bo->handle, (long long)offset, bo->size);
                 abort();
         }
